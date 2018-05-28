36d33c6ab56e51ebbe3e80f84886bcb17e0b3ff7
Christian König <christian.koenig@amd.com>
2018-05-28 11:47:52 +0200

    BACKPORT: dma_buf: remove device parameter from attach callback v2
    The device parameter is completely unused because it is available in the
    attachment structure as well.
    
    v2: fix kerneldoc as well
    
    Change-Id: Iacd7ca570cd03891e15c63f7e1553cbf480f926d
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/226643/
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- drivers/gpu/drm/drm_prime.c
+++ drivers/gpu/drm/drm_prime.c
@@ -181,7 +181,6 @@ static int drm_prime_lookup_buf_handle(struct drm_prime_file_private *prime_fpri
 }
 
 static int drm_gem_map_attach(struct dma_buf *dma_buf,
-			      struct device *target_dev,
 			      struct dma_buf_attachment *attach)
 {
 	struct drm_prime_attachment *prime_attach;
