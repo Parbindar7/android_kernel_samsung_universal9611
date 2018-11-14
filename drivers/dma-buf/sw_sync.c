9da79cc4dddd63c52bdcf5b0f66a9d657d367884
Christian König <christian.koenig@amd.com>
2018-11-14 16:11:06 +0100

    BACKPORT: dma-buf: make fence sequence numbers 64 bit v2
    For a lot of use cases we need 64bit sequence numbers. Currently drivers
    overload the dma_fence structure to store the additional bits.
    
    Stop doing that and make the sequence number in the dma_fence always
    64bit.
    
    For compatibility with hardware which can do only 32bit sequences the
    comparisons in __dma_fence_is_later only takes the lower 32bits as significant
    when the upper 32bits are all zero.
    
    v2: change the logic in __dma_fence_is_later
    
    Change-Id: I735ecc9f20b0fb7b6052c4a73cbe2ea94c0935eb
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Chunming Zhou <david1.zhou@amd.com>
    Link: https://patchwork.freedesktop.org/patch/266927/

--- drivers/dma-buf/sw_sync.c
+++ drivers/dma-buf/sw_sync.c
@@ -176,7 +176,7 @@ static void timeline_fence_disable_signaling(struct dma_fence *fence)
 static void timeline_fence_value_str(struct dma_fence *fence,
 				    char *str, int size)
 {
-	snprintf(str, size, "%d", fence->seqno);
+	snprintf(str, size, "%lld", fence->seqno);
 }
 
 static void timeline_fence_timeline_value_str(struct dma_fence *fence,
